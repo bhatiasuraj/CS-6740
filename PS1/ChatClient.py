634fb51691fb50a3edda4e6c6afd0164f5cffbca
Suraj Bhatia <surajbhatia.sb@gmail.com>
2017-09-10 12:59:15 -0700

    argparse basic working

--- PS1/ChatClient.py
+++ PS1/ChatClient.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python
 
 from socket import *
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", help="USERNAME")
+parser.add_argument("-sip", help="server-ip")
+parser.add_argument("-sp", help="port", type=int)
+args = parser.parse_args()
+
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 message = 'Hello!'
-addr = ("127.0.0.1", 12000)
+addr = (args.sip, args.sp)
 
 clientSocket.sendto(message, addr)
 
--- PS1/ChatServer.py
+++ PS1/ChatServer.py
@@ -3,9 +3,14 @@
 from socket import *
 import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-sp", help="port", type=int)
+args = parser.parse_args()
+print(args.sp)
+
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 
-serverSocket.bind(('', 12000))
+serverSocket.bind(('', args.sp))
 
 while True:
 	message, address = serverSocket.recvfrom(1024)
