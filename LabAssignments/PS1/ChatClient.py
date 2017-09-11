ca94692d89e9087eabb84a88c41c10e4c64bdb3b
Suraj Bhatia <surajbhatia.sb@gmail.com>
2017-09-10 19:07:55 -0700

    Printing one username

--- LabAssignments/PS1/ChatClient.py
+++ LabAssignments/PS1/ChatClient.py
@@ -3,20 +3,44 @@
 from socket import *
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-u", help="USERNAME")
-parser.add_argument("-sip", help="server-ip")
-parser.add_argument("-sp", help="port", type=int)
-args = parser.parse_args()
+def userSignIn(username, addr, s):
 
-addr = (args.sip, args.sp)
+	s.sendto("SIGN-IN", addr)
+	s.sendto(username, addr)
 
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = raw_input("+>")
-if message == "list":
-	clientSocket.sendto("list", addr)
-        data, server = clientSocket.recvfrom(1024)
-	print ("<-", data)
 
-clientSocket.sendto(message, addr)
+def createSocket():
 
+        clientSocket = socket(AF_INET, SOCK_DGRAM)
+	return clientSocket
+
+def userList(addr, s):
+
+	s.sendto("list", addr)
+	data, server = s.recvfrom(1024)
+	print str("<-"+data)
+
+def argsParser():
+
+        parser = argparse.ArgumentParser()
+
+        parser.add_argument("-u", help="USERNAME")
+        parser.add_argument("-sip", help="server-ip")
+        parser.add_argument("-sp", help="port", type=int)
+	args = parser.parse_args()
+
+	return args.u, args.sp, args.sip
+
+def main():
+
+	username, port, ip = argsParser()
+	addr = (ip, port)
+	s = createSocket()
+        userSignIn(username, addr, s)
+	while True:
+	        message = raw_input("+>")
+	        if message == "list":
+			userList(addr, s)
+
+if __name__ == "__main__":
+    main()
--- LabAssignments/PS1/ChatServer.py
+++ LabAssignments/PS1/ChatServer.py
@@ -12,9 +12,18 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', args.sp))
 print("Server Initialized...")
 
+#userList = []
+#username, address = serverSocket.recvfrom(1024)
+#userList.join((username))
+
 while True:
 	message, address = serverSocket.recvfrom(1024)
+	print message
+
+	if message == "SIGN-IN":
+		username, address = serverSocket.recvfrom(1024)
+
 	if message == "list":
-		serverSocket.sendto("Signed in Users: ", address)
+		serverSocket.sendto("Signed in Users: "+username, address)
 
 
