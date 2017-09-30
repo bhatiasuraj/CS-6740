1d274242ba15873b4881a01e001aab4ad6e1d811
Suraj Bhatia <surajbhatia.sb@gmail.com>
2017-09-30 16:40:22 -0700

    Basic encryption/decryption working with CBC mode

--- ProblemSets/PS2/fcrypt.py
+++ ProblemSets/PS2/fcrypt.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 '''
 
@@ -14,10 +14,33 @@ Usage: python fcrypt.py -e destination_public_key_filename sender_private_key_fi
 
 '''
 
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher,algorithms,modes
+from cryptography.hazmat.primitives import hashes, hmac, serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import padding as paddingdatalib
+
 from socket import *
 import argparse
 import sys
+import os
 
+def AESEncryption():
+	
+	key = os.urandom(32)
+	iv = os.urandom(16)
+
+	data = "This is a secret message"
+
+	cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+ 	encryptor = cipher.encryptor()
+    	
+	ct = encryptor.update(b"a secret message") + encryptor.finalize()
+	print ct
+	decryptor = cipher.decryptor()
+	pt = decryptor.update(ct) + decryptor.finalize()
+	print pt
+	
 def argsParser():
 
 	# Command-line arguments parser
@@ -43,21 +66,25 @@ def argsParser():
 	
 def main():
 
-	# Retrieve username, server port and IP from command-line
+	# Retrieve parameter list for encryption/decryption operation from command-line
 	
 	paramList, operation  = argsParser()
 	
-	if operation = "e":
+	if operation == "e":
 		destPubKeyFile = paramList[0]
 		sendPriKeyFile = paramList[1]
 		ipPlainText = paramList[2]
 		cipherFile = paramList[3]
 
-	if operation = "d":
+	if operation == "d":
 		destPriKeyFile = paramList[0]
 		sendPubKeyFile = paramList[1]
 		cipherFile = paramList[2]
 		opPlainText = paramList[3]
+
+	AESEncryption()
+
+
 
 if __name__ == "__main__":
     main()
