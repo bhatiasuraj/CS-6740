e627fb888f454b153a8fd1f00ab02947d68ed4ec
Suraj Bhatia <surajbhatia.sb@gmail.com>
2017-10-06 18:32:37 -0700

    Some more error handling

--- ProblemSets/PS2/testScript.sh
+++ ProblemSets/PS2/testScript.sh
@@ -4,11 +4,11 @@
 
 > outputPlainFile.txt
 
-#python fcrypt.py -e destinationPublicKey.der senderPrivateKey.der inputPlainFile.txt outputCipherFile.txt
-#python fcrypt.py -d destinationPrivateKey.der senderPublicKey.der outputCipherFile.txt outputPlainFile.txt
+python fcrypt.py -e destinationPublicKey.der senderPrivateKey.der inputPlainFile.txt outputCipherFile.txt
+python fcrypt.py -d destinationPrivateKey.der senderPublicKey.der outputCipherFile.txt outputPlainFile.txt
 
-python fcrypt.py -e destinationPublicKey.pem senderPrivateKey.pem inputPlainFile.txt outputCipherFile.txt
-python fcrypt.py -d destinationPrivateKey.pem senderPublicKey.pem outputCipherFile.txt outputPlainFile.txt
+#python fcrypt.py -e destinationPublicKey.pem senderPrivateKey.pem inputPlainFile.txt outputCipherFile.txt
+#python fcrypt.py -d destinationPrivateKey.pem senderPublicKey.pem outputCipherFile.txt outputPlainFile.txt
 
 if [ $? -eq 0 ]; then
 
